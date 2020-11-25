d3467eb59497aee8600a49587a04d86489d8cbfe
elves brito <elvesbd@yahoo.com>
2020-11-24 21:24:29 -0300

    events modal

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let cart = [];
 let modalQtd = 1;
+let modalkey = 0;
 
 const c = (e) => {
   return document.querySelector(e);
@@ -23,6 +25,7 @@ pizzaJson.map((item, index) => {
 
     let key = e.target.closest('.pizza-item').getAttribute('data-key');
     modalQtd = 1;
+    modalkey = key;
 
     c('.pizzaBig img').src = pizzaJson[key].img;
     c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -48,4 +51,46 @@ pizzaJson.map((item, index) => {
 
 
   c('.pizza-area').append(pizzaItem);
+});
+
+//events modal
+function closeModal() {
+  c('.pizzaWindowArea').style.opacity = 0;
+  setTimeout( () => {
+    c('.pizzaWindowArea').style.display = 'none';
+  }, 500);
+};
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach( (item) => {
+  item.addEventListener('click', closeModal)
+});
+
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+  if(modalQtd > 1) {
+    modalQtd--;
+    c('.pizzaInfo--qt').innerHTML = modalQtd;
+  }
+});
+
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+  modalQtd++;
+  c('.pizzaInfo--qt').innerHTML = modalQtd;
+});
+
+cs('.pizzaInfo--size').forEach( (size, sizeIndex) => {
+  size.addEventListener('click', (e) => {
+    c('.pizzaInfo--size.selected').classList.remove('selected');
+    size.classList.add('selected');
+  });
+});
+
+c('.pizzaInfo--addButton').addEventListener('click', () => {
+  let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+  
+  cart.push({
+    id: pizzaJson[modalkey].id,
+    qtd: modalQtd,
+    size
+  });
+  closeModal();
 });
